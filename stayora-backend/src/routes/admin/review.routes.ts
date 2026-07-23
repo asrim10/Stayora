ac3b146d9488f667793693c14def73cd728e84c1
asrim10 <asrimmsuwall@gmail.com>
2026-07-23 10:43:50 +0545

    added missing adminMiddleware in admin review routes

--- stayora-backend/src/routes/admin/review.routes.ts
+++ stayora-backend/src/routes/admin/review.routes.ts
@@ -1,29 +1,35 @@
 import { Router } from "express";
-import { authorizedMiddleware } from "../../middlewares/authorized.middleware";
+import {
+  authorizedMiddleware,
+  adminMiddleware,
+} from "../../middlewares/authorized.middleware";
 import { AdminReviewController } from "../../controllers/admin/review.controller";
 
 const router = Router();
 const adminReviewController = new AdminReviewController();
 
-router.get("/", authorizedMiddleware, (req, res) =>
+router.use(authorizedMiddleware);
+router.use(adminMiddleware);
+
+router.get("/", (req, res) =>
   adminReviewController.getAllReviews(req, res),
 );
-router.get("/:id", authorizedMiddleware, (req, res) =>
+router.get("/:id", (req, res) =>
   adminReviewController.getReviewById(req, res),
 );
-router.get("/user/:userId", authorizedMiddleware, (req, res) =>
+router.get("/user/:userId", (req, res) =>
   adminReviewController.getReviewsByUserId(req, res),
 );
-router.get("/hotel/:hotelId", authorizedMiddleware, (req, res) =>
+router.get("/hotel/:hotelId", (req, res) =>
   adminReviewController.getReviewsByHotelId(req, res),
 );
-router.put("/:id", authorizedMiddleware, (req, res) =>
+router.put("/:id", (req, res) =>
   adminReviewController.updateReview(req, res),
 );
-router.delete("/:id", authorizedMiddleware, (req, res) =>
+router.delete("/:id", (req, res) =>
   adminReviewController.deleteReview(req, res),
 );
-router.get("/stats/all", authorizedMiddleware, (req, res) =>
+router.get("/stats/all", (req, res) =>
   adminReviewController.getReviewStats(req, res),
 );
 
